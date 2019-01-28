668bc4109176d9cc68fd5a50c9bbf77358363a59
sharkinsspatial <sharkinsgis@gmail.com>
2019-01-28 17:55:31 -0500

    Add validation for date fields.

--- src/components/QueryForm.js
+++ src/components/QueryForm.js
@@ -135,6 +135,16 @@ const EnhancedForm = withFormik({
     if (values.startdatetime >= values.enddatetime) {
       errors.startdatetime = 'Start Date must be before End Date';
     }
+    const dateRegex = /^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9])?$/;
+    const invalidDateMessage = 'Must be a valid date and time';
+    const startValid = dateRegex.test(values.startdatetime);
+    if (!startValid) {
+      errors.startdatetime = invalidDateMessage;
+    }
+    const endValid = dateRegex.test(values.enddatetime);
+    if (!endValid) {
+      errors.enddatetime = invalidDateMessage;
+    }
     return errors;
   },
 
